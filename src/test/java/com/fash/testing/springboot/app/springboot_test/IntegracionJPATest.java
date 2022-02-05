05713ccdd4d69d3bd21ee72bafad6473f8dfe1f8
Heriberto Fabian Santiago <fabiansantiago87@gmail.com>
2022-02-05 13:43:26 -0600

    Pruebas de integracion con JPA metodos peronalizados

--- src/test/java/com/fash/testing/springboot/app/springboot_test/IntegracionJPATest.java
+++ src/test/java/com/fash/testing/springboot/app/springboot_test/IntegracionJPATest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,4 +24,25 @@ public class IntegracionJPATest {
         assertEquals("Andres", cuenta.orElseThrow().getPersona());
     }
 
+    @Test
+    void findByPerson() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Andres");
+        assertTrue(cuenta.isPresent());
+        assertEquals("Andres", cuenta.orElseThrow().getPersona());
+        assertEquals("1000.00", cuenta.orElseThrow().getSaldo().toPlainString());
+    }
+
+    @Test
+    void findByPersonThrowException() {
+        Optional<Cuenta> cuenta = cuentaRepository.findByPersona("Rod");
+        assertThrows(NoSuchElementException.class, cuenta::orElseThrow);
+        assertTrue(!cuenta.isPresent());
+    }
+
+    @Test
+    void testFindAll(){
+        List<Cuenta> cuentas = cuentaRepository.findAll();
+        assertFalse(cuentas.isEmpty());
+        assertEquals(2, cuentas.size());
+    }
 }
