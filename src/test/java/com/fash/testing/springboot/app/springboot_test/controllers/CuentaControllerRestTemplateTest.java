c4aad3c908803b5bc721657611af5024056deca4
Heriberto Fabian Santiago <fabiansantiago87@gmail.com>
2022-03-14 18:07:39 -0600

    Pruebas de integracion para listar

--- src/test/java/com/fash/testing/springboot/app/springboot_test/controllers/CuentaControllerRestTemplateTest.java
+++ src/test/java/com/fash/testing/springboot/app/springboot_test/controllers/CuentaControllerRestTemplateTest.java
@@ -1,5 +1,6 @@
 package com.fash.testing.springboot.app.springboot_test.controllers;
 
+import com.fash.testing.springboot.app.springboot_test.models.Cuenta;
 import com.fash.testing.springboot.app.springboot_test.models.TransaccionDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -15,7 +16,9 @@ import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,7 +51,7 @@ class CuentaControllerRestTemplateTest {
 
         //When
         ResponseEntity<String> response = client.
-                postForEntity(crearUri()+"/api/cuentas/transferir", dto, String.class);
+                postForEntity(crearUri("/api/cuentas/transferir"), dto, String.class);
                 //postForEntity("/api/cuentas/transferir", dto, String.class);
 
         String json = response.getBody();
@@ -75,8 +78,58 @@ class CuentaControllerRestTemplateTest {
 
 
 
-    private String crearUri()
+    private String crearUri(String path)
     {
-        return "http://localhost:"+puerto;
+        return "http://localhost:"+puerto + path;
+    }
+
+    @Test
+    @Order(2)
+    void testDetalle() {
+        ResponseEntity<Cuenta> respuesta = client.getForEntity(crearUri("/api/cuentas/1"), Cuenta.class);
+        Cuenta cuenta = respuesta.getBody();
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
+
+        assertNotNull(cuenta);
+        assertEquals("Andres", cuenta.getPersona());
+        assertEquals(1L, cuenta.getId());
+        assertEquals("900.00", cuenta.getSaldo().toPlainString());
+        assertEquals(new Cuenta(1L, "Andres", new BigDecimal("900.00")), cuenta);
+
+    }
+
+    @Test
+    @Order(3)
+    void testListar() throws JsonProcessingException {
+        ResponseEntity<Cuenta[]> respuesta = client.getForEntity(crearUri("/api/cuentas"), Cuenta[].class);
+        List<Cuenta> cuentas = Arrays.asList(respuesta.getBody());
+
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
+
+        assertNotNull(cuentas);
+
+        assertEquals(2, cuentas.size());
+        assertEquals(new Cuenta(1L, "Andres", new BigDecimal("900.00")), cuentas.get(0));
+        assertEquals(new Cuenta(2L, "John", new BigDecimal("2100.00")), cuentas.get(1));
+
+        JsonNode json = mapper.readTree(mapper.writeValueAsString(cuentas));
+
+        assertEquals(1L, json.get(0).path("id").asLong());
+        assertEquals("Andres", json.get(0).path("persona").asText());
+        assertEquals("900.0", json.get(0).path("saldo").asText());
+
+        assertEquals(2L, json.get(1).path("id").asLong());
+        assertEquals("John", json.get(1).path("persona").asText());
+        assertEquals("2100.0", json.get(1).path("saldo").asText());
+
+
     }
 }
+
+
+
+
+
+
